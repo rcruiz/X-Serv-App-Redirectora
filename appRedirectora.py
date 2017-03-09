e1acd5c8bb0b2c795a9daafbad5e676c5624cf37
rcruiz <rc.ruiz@alumnos.urjc.es>
2017-03-09 22:42:31 +0100

    Hecho

--- appRedirectora.py
+++ appRedirectora.py
@@ -1,29 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-"""
-Aplicacion redirectora
-Rosa Cristina Ruiz Rivas
-"""
 
 import webapp
 import random
 
 
 class redirectora(webapp.webApp):
-    """Address with classes"""
 
     def process(self, parsedRequest):
-        """Process the relevant elements of the request.
 
-        Returns 200 OK and an HTML page.
-        """
-        #recurso = parsedRequest[1]
         randomURL = str(random.randint(0, 1000000000))
         newURL = "http://localhost:1234/" + randomURL
         # Espera 5 segs antes de redirigir
-        headHtml = '<meta http-equiv="Refresh" content="5;url=' + newURL + '">'
+        headHtml = '<meta http-equiv="Refresh" content="3;url=' + newURL + '">'
         codigoHTTP = "303 See Others"
-        cuerpoHtml = "<p>Va a ser redirigido en 5 segundos a --> " + newURL
+        cuerpoHtml = "<p>Va a ser redirigido en 3 segundos a --> " + newURL
         cuerpoHtml += "</p>"
         return (codigoHTTP, "<HTML> <HEAD>" + headHtml + "</HEAD> <BODY>" +
                 cuerpoHtml + "</BODY></HTML>")
